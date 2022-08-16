cef1dc78354add9ab50a37971b64a2a46656623a
Md Hedayet Ullah <diupchedayet@gmail.com>
2022-08-16 14:52:53 +0600

    all time data load

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                {{-- {{$newData}} --}}
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
